2bf294e3f3874019b0c52b478ca322cf2a30284b
Keyvan Mir Mohammad Sadeghi <keyvan.m.sadeghi@gmail.com>
2019-07-12 04:39:07 -0400

    Structure Assister Chat project

---
+++ packages/chat/src/components/assister-chat/assister-chat.tsx
@@ -0,0 +1,59 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'assister-chat',
+  styleUrl: 'assister-chat.css'
+})
+export class AssisterChat {
+
+  render() {
+    return [
+      <ion-header>
+        <ion-toolbar color="primary">
+          <ion-title>Home</ion-title>
+        </ion-toolbar>
+      </ion-header>,
+
+      <ion-content class="ion-padding">
+
+      <ion-button href="/profile/ionic" expand="block">Profile page</ion-button>
+
+      <ion-list>
+        <ion-card>
+          <ion-card-content>
+            sege
+          </ion-card-content>
+        </ion-card>
+        <ion-card>
+          <ion-card-content>
+            soote
+          </ion-card-content>
+        </ion-card>
+        <ion-card>
+          <ion-card-content>
+            khare
+          </ion-card-content>
+        </ion-card>
+        <ion-card>
+          <ion-card-content>
+            gave
+          </ion-card-content>
+        </ion-card>
+        <ion-card>
+          <ion-card-content>
+            shaghale
+          </ion-card-content>
+        </ion-card>
+      </ion-list>
+
+      <ion-infinite-scroll threshold="100px" id="infinite-scroll">
+        <ion-infinite-scroll-content loading-spinner="bubbles" loading-text="Loading more data...">
+        </ion-infinite-scroll-content>
+      </ion-infinite-scroll>
+      
+      </ion-content>
+
+      
+    ];
+  }
+}
